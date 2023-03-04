3d37755dae1c153b85b238a442a9443d8416653f
Paulo Geliandes <geliandes@hotmail.com>
2023-03-04 15:25:23 -0300

    add comments

--- Exercício 4/JavaScript- Distribuidora/distribuidora.js
+++ Exercício 4/JavaScript- Distribuidora/distribuidora.js
@@ -1,3 +1,4 @@
+// Criação de uma matriz de objetos com as informações do estado e valor de faturamento;
 const invoicing = [
     {
         "state": "SP",
@@ -24,16 +25,20 @@ const invoicing = [
 let totalValue = 0;
 let percentage = [];
 
+// Iterando o array e somando os valores para obter o valor total e armazenando dentro da variável totalValue
  invoicing.forEach((data) => {
     totalValue += data.value
  })
 
- invoicing.forEach((data, index) => {
+// Iterando o array e inserindo dentro de cada objeto a porcentagem que cada respectivo estado representa
+invoicing.forEach((data, index) => {
     invoicing[index].percentOfTotal = calculatePercentage(data.value, totalValue)
  })
 
+ // Função que calcula a porcentagem que cada estado representa, ela recebe 2 parâmentros, sendo o primeiro parãmetro o valor individual de faturamento e o segundo valor o total de faturamento
  function calculatePercentage(value, total){
     return ((value * 100) / total).toFixed(2);
  }
 
+ // Exibindo as informações
  console.log(invoicing);
